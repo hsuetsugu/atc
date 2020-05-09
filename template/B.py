94204003778731e2bb6dae1678b26369d49bddb6
hsuetsugu <la_80kk@hotmail.com>
2020-05-09 09:53:34 +0900

    update template

--- template/B.py
+++ template/B.py
@@ -2,13 +2,12 @@
 # B
 
 import sys
+from collections import defaultdict, deque
+from heapq import heappush, heappop
+import math
+import bisect
+
 # 再起回数上限変更
 # sys.setrecursionlimit(1000000)
 
 N, M = map(int, input().split())
-A = list(map(int, input().split()))
-
-if N >= sum(A):
-    print(N-sum(A))
-else:
-    print('-1')
--- template/E.py
+++ template/E.py
@@ -2,6 +2,11 @@
 # E
 
 import sys
+from collections import defaultdict, deque
+from heapq import heappush, heappop
+import math
+import bisect
+
 # 再起回数上限変更
 # sys.setrecursionlimit(1000000)
 
