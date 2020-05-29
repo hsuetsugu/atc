fd0523f9e0d09b28320ff7f9b28c42111eb9ef00
hsuetsugu <la_80kk@hotmail.com>
2020-05-29 16:08:34 +0900

    modify templates

--- template/A.py
+++ template/A.py
@@ -6,6 +6,7 @@ from collections import defaultdict, deque
 from heapq import heappush, heappop
 import math
 import bisect
+input = sys.stdin.readline
 
 # 再起回数上限変更
 # sys.setrecursionlimit(1000000)
